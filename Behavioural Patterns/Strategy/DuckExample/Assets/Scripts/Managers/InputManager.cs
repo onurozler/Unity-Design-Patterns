97175cf2452729b6e108e8eb7f7e365a4580338c
onurozler <onur.ozler@ug.bilkent.edu.tr>
2020-01-22 21:45:02 +0300

    Strategy Update

---
+++ Behavioural Patterns/Strategy/DuckExample/Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Duck;
+using UnityEngine;
+using View;
+
+namespace Managers
+{
+    public class InputManager : MonoBehaviour
+    {
+        public Camera Camera;
+
+        private void Awake()
+        {
+            Camera = Camera == null ? Camera.main : Camera;
+        }
+
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                RaycastHit hit;
+                Ray ray = Camera.ScreenPointToRay(Input.mousePosition);
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.collider.GetComponent<DuckBase>())
+                    {
+                        DuckBase duck = hit.collider.GetComponent<DuckBase>();
+                        DuckInfoView.Instance.Open(duck);
+                    }
+                }
+            }
+        }
+    }
+}
+
---
+++ Behavioural Patterns/Strategy/DuckExample/Assets/Scripts/View/DuckInfoView.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Duck;
+using Duck_Behaviors;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+namespace View
+{
+    public class DuckInfoView : MonoBehaviour
+    {
+        [SerializeField] private Text _duckName;
+        [SerializeField] private Text _duckType;
+        [SerializeField] private Text _flyType;
+        [SerializeField] private Button _closeButton;
+        [SerializeField] private Button _changeButton;
+
+        private List<IFlyBehaviour> _flyBehaviours = new List<IFlyBehaviour>()
+        {
+            new FlyNormal(),
+            new FlyZigzag(),
+            new FlyNoWay()
+        };
+        
+        public static DuckInfoView Instance;
+        private void Awake()
+        {
+            _closeButton.onClick.AddListener(Close);
+
+            if (Instance == null)
+                Instance = this;
+            
+            gameObject.SetActive(false);
+        }
+        
+        public void Open(DuckBase duck)
+        {
+            gameObject.SetActive(true);
+            
+            _changeButton.onClick.RemoveAllListeners();
+
+            _duckName.text = "Duck Name : " + duck.Name;
+            _duckType.text = "Duck Type : " + duck.DuckType;
+            _flyType.text = "Fly Type : " + duck.GetFlyBehavior().GetType().Name;
+            
+            _changeButton.onClick.AddListener(() =>
+            {
+                var newFlyBehavior = _flyBehaviours.Skip(Random.Range(0,2))
+                    .FirstOrDefault(s => s.GetType() != duck.GetFlyBehavior().GetType());
+                
+                duck.SetFlyBehavior(newFlyBehavior);
+                _flyType.text = "Fly Type : " + duck.GetFlyBehavior().GetType().Name;
+            });
+
+        }
+        
+        
+        private void Close()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
+
